b2cdb25360c031c83db0e0805f72cb841e7e3007
hravr <agrinkiv74@gmail.com>
2020-08-30 14:39:44 +0300

    123

--- src/pages/EndedOrdersPage/EndedOrdersPage.js
+++ src/pages/EndedOrdersPage/EndedOrdersPage.js
@@ -54,6 +54,15 @@ const EndedOrdersPage = () => {
           orderFinishPlace="ul. Lewakowskiego 12/55"
           orderPrice="28"
         />
+        <ActiveOrders
+          orderNumber="11"
+          orderType="Paczka"
+          orderWeight="21"
+          orderTime="14:88"
+          orderStartPlace="ul. Rynek 3"
+          orderFinishPlace="ul. Lewakowskiego 12/55"
+          orderPrice="28"
+        />
       </div>
     </div>
   );

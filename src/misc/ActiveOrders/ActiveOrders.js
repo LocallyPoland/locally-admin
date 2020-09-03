2cbf24085431802a464a3aeb7857346364c29e81
californiandreamer <sydyahanazar777@gmail.com>
2020-09-03 12:18:47 +0300

    modal_design

--- src/misc/ActiveOrders/ActiveOrders.js
+++ src/misc/ActiveOrders/ActiveOrders.js
@@ -6,19 +6,19 @@ import { getOrdersAction } from "../../store/actions/orderActions";
 import { connect } from "react-redux";
 
 const ActiveOrders = ({
-  orderNumber,
-  orderType,
-  orderWeight,
-  orderTime,
-  orderStartPlace,
-  orderFinishPlace,
-  orderPrice,
-  getOrders,
+    orderNumber,
+    orderType,
+    orderWeight,
+    orderTime,
+    orderStartPlace,
+    orderFinishPlace,
+    orderPrice,
+    getOrders,
 }) => {
-  return (
-    <div className={s.active__orders}>
-      <div className={s.active__orders__inner}>
-        {/* {orders.map((order, i) => {
+    return (
+        <div className={s.active__orders}>
+            <div className={s.active__orders__inner}>
+                {/* {orders.map((order, i) => {
               const {
                 _id,
                 createdAt,
@@ -27,57 +27,83 @@ const ActiveOrders = ({
                 status,
                 products,
               } = order; */}
-        <ActiveOrderWrapper>
-          <div className={s.card__inner}>
-            <div className={s.card__header}>
-              <div className={s.card__title}>Zamówienie n.{orderNumber}</div>
-              <div className={s.card__status}>w trakcie</div>
+                <ActiveOrderWrapper>
+                    <div className={s.card__inner}>
+                        <div className={s.card__header}>
+                            <div className={s.card__title}>
+                                Zamówienie n.{orderNumber}
+                            </div>
+                            <div className={s.card__status}>w trakcie</div>
+                        </div>
+                        <div className={s.card__content}>
+                            <div className={s.card__content__top}>
+                                <div className={s.card__order__type}>
+                                    {orderType}
+                                </div>
+                                <div className={s.card__order__info}>
+                                    <div className={s.card__order__weight}>
+                                        <div className={s.order__weight__text}>
+                                            waga do:
+                                        </div>
+                                        <div className={s.order__weight__value}>
+                                            {orderWeight} kg
+                                        </div>
+                                    </div>
+                                    <div className={s.card__order__time}>
+                                        <div className={s.order__time__text}>
+                                            czas dostawy:
+                                        </div>
+                                        <div className={s.order__time__value}>
+                                            {orderTime}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={s.card__content__bottom}>
+                                {/* <div className={s.card__order__type}>
+                                    Adresy
+                                </div> */}
+                                <div className={s.card__order__adress__wrapper}>
+                                    <div className={s.card__order__from__to}>
+                                        <div className={s.card__order__from}>
+                                            z
+                                        </div>
+                                        <div className={s.card__order__to}>
+                                            do
+                                        </div>
+                                    </div>
+                                    <img
+                                        className={
+                                            s.card__order__connector__img
+                                        }
+                                        src={ConnectorImg}
+                                        alt="connectorImg"
+                                    />
+                                    <div className={s.card__order__adress}>
+                                        <div
+                                            className={
+                                                s.card__order__start__adress
+                                            }
+                                        >
+                                            {orderStartPlace}
+                                        </div>
+                                        <div
+                                            className={
+                                                s.card__order__end__adress
+                                            }
+                                        >
+                                            {orderFinishPlace}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={s.card__price}>{orderPrice} zł</div>
+                        </div>
+                    </div>
+                </ActiveOrderWrapper>
             </div>
-            <div className={s.card__content}>
-              <div className={s.card__content__top}>
-                <div className={s.card__order__type}>{orderType}</div>
-                <div className={s.card__order__info}>
-                  <div className={s.card__order__weight}>
-                    <div className={s.order__weight__text}>waga do:</div>
-                    <div className={s.order__weight__value}>
-                      {orderWeight} kg
-                    </div>
-                  </div>
-                  <div className={s.card__order__time}>
-                    <div className={s.order__time__text}>czas dostawy:</div>
-                    <div className={s.order__time__value}>{orderTime}</div>
-                  </div>
-                </div>
-              </div>
-              <div className={s.card__content__bottom}>
-                <div className={s.card__order__type}>Adresy</div>
-                <div className={s.card__order__adress__wrapper}>
-                  <div className={s.card__order__from__to}>
-                    <div className={s.card__order__from}>z</div>
-                    <div className={s.card__order__to}>do</div>
-                  </div>
-                  <img
-                    className={s.card__order__connector__img}
-                    src={ConnectorImg}
-                    alt="connectorImg"
-                  />
-                  <div className={s.card__order__adress}>
-                    <div className={s.card__order__start__adress}>
-                      {orderStartPlace}
-                    </div>
-                    <div className={s.card__order__end__adress}>
-                      {orderFinishPlace}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className={s.card__price}>{orderPrice} zł</div>
-            </div>
-          </div>
-        </ActiveOrderWrapper>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 // const mapStateToProps = (state) => {
 //   return { orders: state.admin.orders };
--- src/misc/Statistics/Statistics.js
+++ src/misc/Statistics/Statistics.js
@@ -7,136 +7,159 @@ import { connect } from "react-redux";
 import { getStatsAction } from "../../store/actions/statsAction";
 
 const Statistics = ({
-  profitPrice,
-  profitQuarterPrice,
-  profitYearPrice,
-  profitFullprice,
-  userMonthQuarantine,
-  userQuarterQuarantine,
-  userYearQuarantine,
-  userFullQuarantine,
-  orderMonthQuarantine,
-  orderQuarterQuarantine,
-  orderYearQuarantine,
-  orderFullQuarantine,
-  getStats,
-  stats,
+    profitPrice,
+    profitQuarterPrice,
+    profitYearPrice,
+    profitFullprice,
+    userMonthQuarantine,
+    userQuarterQuarantine,
+    userYearQuarantine,
+    userFullQuarantine,
+    orderMonthQuarantine,
+    orderQuarterQuarantine,
+    orderYearQuarantine,
+    orderFullQuarantine,
+    getStats,
+    stats,
 }) => {
-  // const { ordersLengthPerMonth } = stats;
-  useEffect(() => {
-    (async () => {
-      await getStats();
-    })();
-  }, []);
-  console.log("stats ===", stats);
-  return (
-    <div className={s.statistics__inner}>
-      <div className={s.statistics__buttons__row}>
-        <div className={s.statistics__button__item}>
-          <ActiveOrderWrapper>
-            <div className={s.statistics__button__item__inner}>
-              <div className={s.statistics__title}>Zysk</div>
-              <div className={s.statistic__content}>
-                <div className={s.statistic__row}>
-                  <div className={s.statistic__name}>Miesiąc:</div>
-                  <div className={s.statistic__value}>
-                    {stats.data.ordersSumPerMonth} zł
-                  </div>
+    // const { ordersLengthPerMonth } = stats;
+    useEffect(() => {
+        (async () => {
+            await getStats();
+        })();
+    }, []);
+    console.log("stats ===", stats);
+    return (
+        <div className={s.statistics__inner}>
+            <div className={s.statistics__buttons__row}>
+                <div className={s.statistics__button__item}>
+                    <ActiveOrderWrapper>
+                        <div className={s.statistics__button__item__inner}>
+                            <div className={s.statistics__title}>Zysk</div>
+                            <div className={s.statistic__content}>
+                                <div className={s.statistic__row}>
+                                    <div className={s.statistic__name}>
+                                        Miesiąc:
+                                    </div>
+                                    <div className={s.statistic__value}>
+                                        {stats.data.ordersSumPerMonth} zł
+                                    </div>
+                                </div>
+                                <div className={s.statistic__row}>
+                                    <div className={s.statistic__name}>
+                                        Kwartal:
+                                    </div>
+                                    <div className={s.statistic__value}>
+                                        {stats.data.ordersSumPerQuarter} zł
+                                    </div>
+                                </div>
+                                <div className={s.statistic__row}>
+                                    <div className={s.statistic__name}>
+                                        Rok:
+                                    </div>
+                                    <div className={s.statistic__value}>
+                                        {stats.data.ordersSumPerYear} zł
+                                    </div>
+                                </div>
+                                <div className={s.statistic__among}>
+                                    {stats.data.ordersSumPerAllTime} zł
+                                </div>
+                            </div>
+                        </div>
+                    </ActiveOrderWrapper>
                 </div>
-                <div className={s.statistic__row}>
-                  <div className={s.statistic__name}>Kwartal:</div>
-                  <div className={s.statistic__value}>
-                    {stats.data.ordersSumPerQuarter} zł
-                  </div>
+                <div className={s.statistics__button__item}>
+                    <ActiveOrderWrapper>
+                        <div className={s.statistics__button__item__inner}>
+                            <div className={s.statistics__title}>
+                                Użytkowniki
+                            </div>
+                            <div className={s.statistic__content}>
+                                <div className={s.statistic__row}>
+                                    <div className={s.statistic__name}>
+                                        Miesiąc:
+                                    </div>
+                                    <div className={s.statistic__value}>
+                                        {userMonthQuarantine}
+                                    </div>
+                                </div>
+                                <div className={s.statistic__row}>
+                                    <div className={s.statistic__name}>
+                                        Kwartal:
+                                    </div>
+                                    <div className={s.statistic__value}>
+                                        {userQuarterQuarantine}
+                                    </div>
+                                </div>
+                                <div className={s.statistic__row}>
+                                    <div className={s.statistic__name}>
+                                        Rok:
+                                    </div>
+                                    <div className={s.statistic__value}>
+                                        {userYearQuarantine}
+                                    </div>
+                                </div>
+                                <div className={s.statistic__among}>
+                                    {userFullQuarantine} uż.
+                                </div>
+                            </div>
+                        </div>
+                    </ActiveOrderWrapper>
                 </div>
-                <div className={s.statistic__row}>
-                  <div className={s.statistic__name}>Rok:</div>
-                  <div className={s.statistic__value}>
-                    {stats.data.ordersSumPerYear} zł
-                  </div>
+                <div className={s.statistics__button__item}>
+                    <ActiveOrderWrapper>
+                        <div className={s.statistics__button__item__inner}>
+                            <div className={s.statistics__title}>
+                                Zamówienia
+                            </div>
+                            <div className={s.statistic__content}>
+                                <div className={s.statistic__row}>
+                                    <div className={s.statistic__name}>
+                                        Miesiąc:
+                                    </div>
+                                    <div className={s.statistic__value}>
+                                        {stats.data.ordersLengthPerMonth}
+                                    </div>
+                                </div>
+                                <div className={s.statistic__row}>
+                                    <div className={s.statistic__name}>
+                                        Kwartal:
+                                    </div>
+                                    <div className={s.statistic__value}>
+                                        {stats.data.ordersLengthPerQuarter}
+                                    </div>
+                                </div>
+                                <div className={s.statistic__row}>
+                                    <div className={s.statistic__name}>
+                                        Rok:
+                                    </div>
+                                    <div className={s.statistic__value}>
+                                        {stats.data.ordersLengthPerYear}
+                                    </div>
+                                </div>
+                                <div className={s.statistic__among}>
+                                    {stats.data.ordersLengthPerAllTime} zamówień
+                                </div>
+                            </div>
+                        </div>
+                    </ActiveOrderWrapper>
                 </div>
-                <div className={s.statistic__among}>
-                  {stats.data.ordersSumPerAllTime} zł
-                </div>
-              </div>
             </div>
-          </ActiveOrderWrapper>
-        </div>
-        <div className={s.statistics__button__item}>
-          <ActiveOrderWrapper>
-            <div className={s.statistics__button__item__inner}>
-              <div className={s.statistics__title}>Użytkowniki</div>
-              <div className={s.statistic__content}>
-                <div className={s.statistic__row}>
-                  <div className={s.statistic__name}>Miesiąc:</div>
-                  <div className={s.statistic__value}>
-                    {userMonthQuarantine}
-                  </div>
+            <div className={s.chart__container}>
+                <div className={s.card}>
+                    <Chart {...{ stats }} />
                 </div>
-                <div className={s.statistic__row}>
-                  <div className={s.statistic__name}>Kwartal:</div>
-                  <div className={s.statistic__value}>
-                    {userQuarterQuarantine}
-                  </div>
-                </div>
-                <div className={s.statistic__row}>
-                  <div className={s.statistic__name}>Rok:</div>
-                  <div className={s.statistic__value}>{userYearQuarantine}</div>
-                </div>
-                <div className={s.statistic__among}>
-                  {userFullQuarantine} uż.
-                </div>
-              </div>
             </div>
-          </ActiveOrderWrapper>
         </div>
-        <div className={s.statistics__button__item}>
-          <ActiveOrderWrapper>
-            <div className={s.statistics__button__item__inner}>
-              <div className={s.statistics__title}>Zamówienia</div>
-              <div className={s.statistic__content}>
-                <div className={s.statistic__row}>
-                  <div className={s.statistic__name}>Miesiąc:</div>
-                  <div className={s.statistic__value}>
-                    {stats.data.ordersLengthPerMonth}
-                  </div>
-                </div>
-                <div className={s.statistic__row}>
-                  <div className={s.statistic__name}>Kwartal:</div>
-                  <div className={s.statistic__value}>
-                    {stats.data.ordersLengthPerQuarter}
-                  </div>
-                </div>
-                <div className={s.statistic__row}>
-                  <div className={s.statistic__name}>Rok:</div>
-                  <div className={s.statistic__value}>
-                    {stats.data.ordersLengthPerYear}
-                  </div>
-                </div>
-                <div className={s.statistic__among}>
-                  {"vadim piddr"}
-                  zamówień
-                </div>
-              </div>
-            </div>
-          </ActiveOrderWrapper>
-        </div>
-      </div>
-      <div className={s.chart__container}>
-        <EndedOrderWrapper>
-          <Chart {...{ stats }} />
-        </EndedOrderWrapper>
-      </div>
-    </div>
-  );
+    );
 };
 const mapStateToProps = (state) => {
-  return { stats: state.stats };
+    return { stats: state.stats };
 };
 const mapDispatchToProps = (dispatch) => {
-  return {
-    getStats: () => dispatch(getStatsAction()),
-  };
+    return {
+        getStats: () => dispatch(getStatsAction()),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Statistics);

75e7a3974f3a1355773ee23858331bda872c3d9b
vadvit009 <vadvit009@gmail.com>
2020-10-07 21:10:02 +0300

    update delivery address

--- src/misc/ModalWindow/ModalWindow.js
+++ src/misc/ModalWindow/ModalWindow.js
@@ -1,112 +1,125 @@
-import React, { useState, useEffect } from "react";
+import React, {useEffect} from "react";
 import s from "./ModalWindow.module.css";
 import classNames from "class-names";
 import CloseImg from "../../assets/images/close.svg";
 import ActiveOrders from "../ActiveOrders/ActiveOrders";
 import ClientInfo from "../ClientInfo/ClientInfo";
-import { getSingleOrderAction } from "../../store/actions/orderActions";
-import { connect } from "react-redux";
+import {getSingleOrderAction, patchSingleOrderAction} from "../../store/actions/orderActions";
+import {connect} from "react-redux";
 
-const ModalWindow = ({
-  isVisible,
-  setVisibility,
-  id,
-  getSingleOrder,
-  singleOrder,
-}) => {
-  const {
-    _id,
-    parcel,
-    weight,
-    deliveryTime,
-    pickup,
-    deliveryStreet,
-    deliveryHouse,
-    sum,
-    userID,
-    paymentType,
-  } = singleOrder;
-  useEffect(() => {
-    _id &&
-      (async () => {
-        await getSingleOrder(id);
-      })();
-  }, [id]);
+const ModalWindow = (
+    {
+        isVisible,
+        setVisibility,
+        id,
+        getSingleOrder,
+        singleOrder,
+        patchOrder
+    }) => {
 
-  const visible = classNames({
-    [s.modal]: isVisible,
-    [s.visible]: isVisible,
-    [s.hidden]: !isVisible,
-  });
+    const {
+        _id,
+        parcel,
+        weight,
+        deliveryTime,
+        pickup,
+        deliveryAddress,
+        sum,
+        status,
+        userID,
+        paymentType,
+    } = singleOrder;
 
-  const closeModal = () => {
-    document.body.style.overflow = "";
-    setVisibility(false);
-  };
+    useEffect(() => {
+        id &&
+        (async () => {
+            await getSingleOrder(id);
+        })();
+    }, [id]);
 
-  return (
-    !!singleOrder && (
-      <div className={visible} onClick={closeModal}>
-        <div className={s.modal__inner}>
-          <div className={s.modal__container}>
-            <button className={s.modal__close__btn} onClick={closeModal}>
-              <img className={s.modal__close__img} src={CloseImg} alt="close" />
-            </button>
-            <div className={s.modal__left__side}>
-              <div className={s.modal__side__title}>Szczegóły zamówienia</div>
-              <div className={s.modal__details__container}>
-                <ActiveOrders
-                  orderNumber={(_id && _id.length) || "11"}
-                  orderType={parcel || "Paczka"}
-                  orderWeight={weight || "21"}
-                  orderTime={deliveryTime || "14:88"}
-                  orderStartPlace={pickup || "ul. Rynek 3"}
-                  orderFinishPlace={
-                    `${deliveryStreet}, ${deliveryHouse}` ||
-                    "ul. Lewakowskiego 12/55"
-                  }
-                  orderPrice={sum || "28"}
-                />
-              </div>
+    const visible = classNames({
+        [s.modal]: isVisible,
+        [s.visible]: isVisible,
+        [s.hidden]: !isVisible,
+    });
+
+    const closeModal = () => {
+        document.body.style.overflow = "";
+        setVisibility(false);
+    };
+
+    return (
+        !!singleOrder && (
+            <div className={visible} onClick={closeModal}>
+                <div className={s.modal__inner}>
+                    <div className={s.modal__container}>
+                        <button className={s.modal__close__btn} onClick={closeModal}>
+                            <img className={s.modal__close__img} src={CloseImg} alt="close"/>
+                        </button>
+                        <div className={s.modal__left__side}>
+                            <div className={s.modal__side__title}>Szczegóły zamówienia</div>
+                            <div className={s.modal__details__container}>
+                                <ActiveOrders
+                                    orderNumber={(_id && _id.length) || "11"}
+                                    orderType={parcel}
+                                    orderWeight={weight}
+                                    orderTime={deliveryTime}
+                                    orderStartPlace={pickup}
+                                    orderFinishPlace={deliveryAddress}
+                                    orderPrice={sum || "28"}
+                                />
+                            </div>
+                        </div>
+                        <div className={s.modal__right__side}>
+                            <div className={s.modal__right__side__info}>
+                                <div className={s.modal__side__title}>
+                                    Informacje o użytkowniku
+                                </div>
+                                <div className={s.modal__details__container}>
+                                    <ClientInfo {...{userID}} {...{paymentType}} />
+                                </div>
+                            </div>
+                            {status === "created" && <div className={s.modal__right__side__rows__container}>
+                                <div className={s.modal__right__side__row}>
+                                    <button className={s.modal__right__side__complete__btn}
+                                            onClick={() => {
+                                                patchOrder({
+                                                    id,
+                                                    status: 'done'
+                                                })
+                                            }}>
+                                        Zakończyć zamówienie
+                                    </button>
+                                </div>
+                                <div className={s.modal__right__side__row}>
+                                    <button className={s.modal__right__side__cancel__btn}
+                                            onClick={() => {
+                                                patchOrder({
+                                                    id,
+                                                    status: 'canceled'
+                                                })
+                                            }}>
+                                        Skasować zamówienie
+                                    </button>
+                                </div>
+                            </div>}
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={s.modal__right__side}>
-              <div className={s.modal__right__side__info}>
-                <div className={s.modal__side__title}>
-                  Informacje o użytkowniku
-                </div>
-                <div className={s.modal__details__container}>
-                  <ClientInfo {...{ userID }} {...{ paymentType }} />
-                </div>
-              </div>
-              <div className={s.modal__right__side__rows__container}>
-                <div className={s.modal__right__side__row}>
-                  <button className={s.modal__right__side__complete__btn}>
-                    Zakończyć zamówienie
-                  </button>
-                </div>
-                <div className={s.modal__right__side__row}>
-                  <button className={s.modal__right__side__cancel__btn}>
-                    Skasować zamówienie
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  );
+        )
+    );
 };
 
 const mapStateToProps = (state) => {
-  return {
-    singleOrder: state.order.singleOrder,
-  };
+    return {
+        singleOrder: state.order.singleOrder,
+    };
 };
 const mapDispatchToProps = (dispatch) => {
-  return {
-    getSingleOrder: (id) => dispatch(getSingleOrderAction(id)),
-  };
+    return {
+        getSingleOrder: (id) => dispatch(getSingleOrderAction(id)),
+        patchOrder: (data) => dispatch(patchSingleOrderAction(data))
+    };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(ModalWindow);
-// export default ModalWindow;
--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -1,171 +1,164 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import s from "./Layout.module.css";
-import UserAvatar from "../../assets/images/avatar.jpg";
 import ActiveOrders from "../../misc/ActiveOrders/ActiveOrders";
 import SettingsForm from "../../misc/SettingsForm/SettingsForm";
 import Statistics from "../../misc/Statistics/Statistics";
 import ModalWindow from "../../misc/ModalWindow/ModalWindow";
 import ArrowForward from "../../assets/images/arrow-forward.svg";
-import { Link, useHistory } from "react-router-dom";
-import { ReactComponent as LogoutIcon } from "../../assets/images/logout.svg";
-import { logoutAction } from "../../store/actions/adminActions";
-import { connect } from "react-redux";
-import { getOrdersAction } from "../../store/actions/orderActions";
+import {Link, useHistory} from "react-router-dom";
+import {ReactComponent as LogoutIcon} from "../../assets/images/logout.svg";
+import {logoutAction} from "../../store/actions/adminActions";
+import {connect} from "react-redux";
+import {getOrdersAction} from "../../store/actions/orderActions";
 
-const Layout = ({ logout, order: { orders }, getOrders }) => {
-  const [isModalVisible, setModalVisible] = useState(false);
-  const [orderId, setOrderId] = useState();
+const Layout = ({logout, order: {orders}, getOrders}) => {
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [orderId, setOrderId] = useState();
 
-  const h = useHistory();
-  const Logout = () => {
-    logout();
-    h.push("/");
-  };
+    const h = useHistory();
+    const Logout = () => {
+        logout();
+        h.push("/");
+    };
 
-  useEffect(() => {
-    (async () => {
-      await getOrders();
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            await getOrders();
+        })();
+    }, []);
 
-  const showModal = (id) => {
-    document.body.style.overflow = "hidden";
-    setModalVisible(true);
-    setOrderId(id);
-  };
+    const showModal = (id) => {
+        document.body.style.overflow = "hidden";
+        setModalVisible(true);
+        setOrderId(id);
+    };
 
-  return (
-    <div className={s.main__container}>
-      <header className={s.header}>
-        <div className={s.header__inner}>
-          <div className={s.header__title}>Locally admin panel</div>
-          <div className={s.header__user}>
-            <div className={s.header__user__name}>jar02212@gmail.com</div>
-            <div className={s.header__user__avatar}>
-              <LogoutIcon className={s.user__logout} onClick={Logout} />
+    return (
+        <div className={s.main__container}>
+            <header className={s.header}>
+                <div className={s.header__inner}>
+                    <div className={s.header__title}>Locally admin panel</div>
+                    <div className={s.header__user}>
+                        <div className={s.header__user__name}>jar02212@gmail.com</div>
+                        <div className={s.header__user__avatar}>
+                            <LogoutIcon className={s.user__logout} onClick={Logout}/>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <ModalWindow
+                isVisible={isModalVisible}
+                id={orderId}
+                setVisibility={setModalVisible}
+            />
+            <div className={s.orders__container}>
+                <div className={s.orders__container__wrapper}>
+                    <div className={s.order__title__wrapper}>
+                        <div className={s.column__title}>Aktywne zamówienia</div>
+                        <Link className={s.column__link} to="/active-orders">
+                            <span className={s.link__text}>Wszystke</span>
+                            <img
+                                className={s.link__img}
+                                src={ArrowForward}
+                                alt="arrow-forward"
+                            />
+                        </Link>
+                    </div>
+                    <div className={s.orders__column}>
+                        {orders.activeOrders &&
+                        orders.activeOrders.map((active) => {
+                            return (
+                                <button
+                                    key={active._id}
+                                    onClick={() => showModal(active._id)}
+                                    className={s.order__button}
+                                >
+                                    <ActiveOrders
+                                        orderNumber={active._id.length}
+                                        orderType={active.parcel}
+                                        orderWeight={active.weight}
+                                        orderTime={active.deliveryTime}
+                                        orderStartPlace={active.pickUp}
+                                        orderFinishPlace={active.deliveryStreet}
+                                        orderPrice={active.sum}
+                                        orderStatus={active.status}
+                                    />
+                                </button>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className={s.orders__container__wrapper}>
+                    <div className={s.order__title__wrapper}>
+                        <div className={s.column__title}>Historia zamówień</div>
+                        <Link className={s.column__link} to="/ended-orders">
+                            <span className={s.link__text}>Wszystke</span>
+                            <img
+                                className={s.link__img}
+                                src={ArrowForward}
+                                alt="arrow-forward"
+                            />
+                        </Link>
+                    </div>
+                    <div className={s.orders__column}>
+                        {orders.ordersHistory &&
+                        orders.ordersHistory.map((history) => {
+                            return (
+                                <button
+                                    key={history._id}
+                                    onClick={() => showModal(history._id)}
+                                    className={s.order__button}
+                                >
+                                    <ActiveOrders
+                                        orderNumber={history._id.length}
+                                        orderType={history.parcel}
+                                        orderWeight={history.weight}
+                                        orderTime={history.deliveryTime}
+                                        orderStartPlace={history.pickUp}
+                                        orderFinishPlace={history.deliveryAddress}
+                                        orderPrice={history.sum}
+                                        orderStatus={history.status}
+                                    />
+                                </button>
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
-          </div>
+            <div className={s.settings__container}>
+                <div className={s.settings__title}>Ustawienia aplikacji</div>
+                <SettingsForm/>
+            </div>
+            <div className={s.statistics__container}>
+                <div className={s.statistics__title}>Statystyka</div>
+                <Statistics
+                    // profitPrice="123"
+                    // profitQuarterPrice="123"
+                    // profitYearPrice="123"
+                    // profitFullprice="123"
+                    // userMonthQuarantine="123"
+                    // userQuarterQuarantine="132"
+                    // userYearQuarantine="8645"
+                    // userFullQuarantine="123"
+                    // orderMonthQuarantine="58"
+                    // orderQuarterQuarantine="351"
+                    // orderYearQuarantine="45"
+                    // orderFullQuarantine="56"
+                />
+            </div>
         </div>
-      </header>
-      <ModalWindow
-        isVisible={isModalVisible}
-        id={orderId}
-        setVisibility={setModalVisible}
-      />
-      <div className={s.orders__container}>
-        <div className={s.orders__container__wrapper}>
-          <div className={s.order__title__wrapper}>
-            <div className={s.column__title}>Aktywne zamówienia</div>
-            <Link className={s.column__link} to="/active-orders">
-              <span className={s.link__text}>Wszystke</span>
-              <img
-                className={s.link__img}
-                src={ArrowForward}
-                alt="arrow-forward"
-              />
-            </Link>
-          </div>
-          <div className={s.orders__column}>
-            {orders.activeOrders &&
-              orders.activeOrders.map((active) => {
-                return (
-                  <button
-                    key={active._id}
-                    onClick={() => showModal(active._id)}
-                    className={s.order__button}
-                  >
-                    <ActiveOrders
-                      orderNumber={active._id.length}
-                      orderType={active.parcel || "Paczka"}
-                      orderWeight={active.weight || "5"}
-                      orderTime={active.deliveryTime || "12.20"}
-                      orderStartPlace={
-                        active.pickUp ||
-                        `${active.deliveryStreet}, ${active.deliveryHouse}`
-                      }
-                      orderFinishPlace={`${active.deliveryStreet}, ${active.deliveryHouse}`}
-                      orderPrice={active.sum}
-                      orderStatus={active.status}
-                    />
-                  </button>
-                );
-              })}
-          </div>
-        </div>
-        <div className={s.orders__container__wrapper}>
-          <div className={s.order__title__wrapper}>
-            <div className={s.column__title}>Historia zamówień</div>
-            <Link className={s.column__link} to="/ended-orders">
-              <span className={s.link__text}>Wszystke</span>
-              <img
-                className={s.link__img}
-                src={ArrowForward}
-                alt="arrow-forward"
-              />
-            </Link>
-          </div>
-          <div className={s.orders__column}>
-            {orders.ordersHistory &&
-              orders.ordersHistory.map((history) => {
-                return (
-                  <button
-                    key={history._id}
-                    onClick={() => showModal(history._id)}
-                    className={s.order__button}
-                  >
-                    <ActiveOrders
-                      orderNumber={history._id.length}
-                      orderType={history.parcel || "Paczka"}
-                      orderWeight={history.weight || "5"}
-                      orderTime={history.deliveryTime || "12.20"}
-                      orderStartPlace={
-                        history.pickUp ||
-                        `${history.deliveryStreet}, ${history.deliveryHouse}`
-                      }
-                      orderFinishPlace={`${history.deliveryStreet}, ${history.deliveryHouse}`}
-                      orderPrice={history.sum}
-                      orderStatus={history.status}
-                    />
-                  </button>
-                );
-              })}
-          </div>
-        </div>
-      </div>
-      <div className={s.settings__container}>
-        <div className={s.settings__title}>Ustawienia aplikacji</div>
-        <SettingsForm />
-      </div>
-      <div className={s.statistics__container}>
-        <div className={s.statistics__title}>Statystyka</div>
-        <Statistics
-        // profitPrice="123"
-        // profitQuarterPrice="123"
-        // profitYearPrice="123"
-        // profitFullprice="123"
-        // userMonthQuarantine="123"
-        // userQuarterQuarantine="132"
-        // userYearQuarantine="8645"
-        // userFullQuarantine="123"
-        // orderMonthQuarantine="58"
-        // orderQuarterQuarantine="351"
-        // orderYearQuarantine="45"
-        // orderFullQuarantine="56"
-        />
-      </div>
-    </div>
-  );
+    );
 };
 
 const mapStateToProps = (state) => {
-  return { order: state.order };
+    return {order: state.order};
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    logout: () => dispatch(logoutAction()),
-    getOrders: () => dispatch(getOrdersAction()),
-  };
+    return {
+        logout: () => dispatch(logoutAction()),
+        getOrders: () => dispatch(getOrdersAction()),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Layout);
